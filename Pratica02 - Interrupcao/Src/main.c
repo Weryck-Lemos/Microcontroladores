efa1864bfecba112229ef618d332956a8d72b751
Weryck-Lemos <weryyck@gmail.com>
2026-06-10 14:55:59 -0300

    pratica3

--- Pratica02 - Interrupcao/Src/main.c
+++ Pratica02 - Interrupcao/Src/main.c
@@ -6,7 +6,7 @@ void EXTI1_IRQHandler(void);  // PB1
 void EXTI9_5_IRQHandler(void); //PB5 ate PB9
 
 int main(void) {
-    //habilitar clock
+    //habilitar porta a,b e AFIO(interrupcao)
     RCC->APB2ENR |= (1 << 2) | (1 << 3) | (1 << 0);
 
     //PA0, PA1, PA2 output
@@ -14,27 +14,24 @@ int main(void) {
     GPIOA->CRL |=  (0x222);
 
     //PB0, PB1, PB5 input pull-up
-    GPIOB->CRL &= ~( (0xF << 0) | (0xF << 4) | (0xF << 20) );
-    GPIOB->CRL |=  ( (0x8 << 0) | (0x8 << 4) | (0x8 << 20) );
-    GPIOB->ODR |=  (1 << 0) | (1 << 1) | (1 << 5); // ativa Pull-up
+    GPIOB->CRL &= ~( (0xF << 0)|(0xF << 4) | (0xF << 20) );
+    GPIOB->CRL |=  ( (0x8 << 0)|(0x8 << 4) | (0x8 << 20) );
+    GPIOB->ODR |=  (1 << 0) | (1 << 1) |(1 << 5);
 
-    //configurar AFIO para mapear os pinos como EXTI
-    // AFIO->EXTICR[0] pinos 0 a 3. AFIO->EXTICR[1] pinos 4 a 7.
+
     AFIO->EXTICR[0] |= (0x1 << 0);  // mapeia PB0 para EXTI0
     AFIO->EXTICR[0] |= (0x1 << 4);  //mapeia PB1 para EXTI1
     AFIO->EXTICR[1] |= (0x1 << 4);  // mapeia PB5 para EXTI5
 
-    //EXTI
     EXTI->FTSR |= (1 << 0) | (1 << 1) | (1 << 5); // trigger descida 0V
     EXTI->IMR  |= (1 << 0) | (1 << 1) | (1 << 5); // mascara de interrupcao
 
     //habilitar NVIC
     NVIC_EnableIRQ(EXTI0_IRQn);
     NVIC_EnableIRQ(EXTI1_IRQn);
-    NVIC_EnableIRQ(EXTI9_5_IRQn); // PB5 compartilha esse canal com PB6-PB9
+    NVIC_EnableIRQ(EXTI9_5_IRQn);
 
     while(1) {
-
         __WFI();
     }
 }
--- README.md
+++ README.md
@@ -14,6 +14,9 @@ Nesta primeira prática, exploramos os fundamentos dos registros de GPIO (Genera
 ### 2. Interrupções Externas (EXTI) e Manipulação de LED RGB
 A segunda prática introduziu o conceito de interrupções externas (EXTI) e o controlador de interrupções de vetor aninhado (NVIC). Em vez de verificar os botões constantemente, o sistema foi configurado para reagir apenas em eventos de borda de descida (*falling edge*). Utilizamos três botões para alternar o estado (operação XOR/Toggle) de cada cor de um LED RGB, demonstrando como tratar diferentes canais de interrupção (EXTI0, EXTI1 e o canal compartilhado EXTI9_5) e a importância de limpar as *flags* de pendência no registrador PR.
 
+### 3.Temporização por Hardware (SysTick) e Sequenciador de Cores RGB
+Na terceira prática, utilizamos o temporizador interno do núcleo ARM, o SysTick, para criar uma base de tempo precisa sem bloquear a CPU com atrasos vazios. O temporizador foi configurado com um divisor de clock por 8 (AHB/8) para gerar interrupções periódicas a cada 1 segundo. A cada disparo do handler, uma variável contadora avança através de uma sequência de 0 a 7, aplicando uma máscara binária diretamente no registrador ODR da Porta A (pinos PA0, PA1 e PA2) para alternar ciclicamente entre as 8 combinações possíveis de cores do LED RGB.
+
 ---
 
 ## 🛠️ Hardware Utilizado
